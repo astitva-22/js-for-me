2d622075e911b2648b74d0af2710a8c9548082f6
astitva-22 <astitva.23b1531096@abes.ac.in>
2026-02-17 06:24:18 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =1234  // cant be changed
+let email="astitva4567@gmail.com"
+var accountPass ="asti" // var ka use nhi karna h kyuki block scope aur functional scope me dikkat hoti hai 
+accCity ="rob"
+
+// accountId=2 it will give you error because it is const declared
+
+email="as.as@.com"
+accountPass="23"
+
+accCity="ob"
+
+console.log(accountId);
+
+console.table([email,accountPass,accCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name="astitva"
+let age =21
+let isLogged = true
+
+let state;
+
+// number =>2 to pwr 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone mtlb value to hai but khali h 0 nhi h khali hai 
+// undefined => variable to define kar diye but value nhi diye
+// symbol =>unique
+
+//object 
+
+console.table([typeof undefined, typeof null]);
+// dhyan dena undeifined hi rheta h but null ek object hai 
+
+
